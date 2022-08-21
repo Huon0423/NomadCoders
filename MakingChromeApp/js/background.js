1a6fd8dce53a65500bf3e8a1978465e605cefe33
Yunjeong Cho <dbswjd1413@naver.com>
2022-08-22 03:16:00 +0900

    remakeApp1

--- MakingChromeApp/js/background.js
+++ MakingChromeApp/js/background.js
@@ -1,9 +1,10 @@
-const images = ["0.jpeg","1.png","2.jpeg","3.webp","4.png","5.jpeg","6.jpg",];
+const images = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg",
+"8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg"];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
-const bgImage = document.createElement("img");
+const body = document.body;
 
-bgImage.src = `img/${chosenImage}`;
+const url = "C:/workspace/VisualStudio_Workspace/MakingChromeApp/img";
 
-document.body.appendChild(bgImage);
+body.style.backgroundImage = "url('"+url+`/${chosenImage}`+"')";
